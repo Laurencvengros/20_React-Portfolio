5ced7937c9b5e05f44814adc5769306c0125c5fe
Lauren <l.cvengros@icloud.com>
2022-12-07 10:30:32 -0500

    importing the email validator helper and useState into contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { validateEmail } from '../utils.js/helper';
+
+
 
 const styles ={
     contactHeader:{
