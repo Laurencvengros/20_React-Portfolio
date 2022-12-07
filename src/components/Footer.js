7436c39a850c2c6c76d8e17fea8894296a153870
Lauren <l.cvengros@icloud.com>
2022-12-07 14:57:08 -0500

    added icon links to other platforms

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,31 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
 import {  FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 
 function Footer() {
     return (
         <React.Fragment>
-        
+        <IconContext.Provider value={{ color: '#ffbb00', size: '2em' }}>
         <footer class="footer" >
-      
+            <div >
+                <a href= 'mailto:l.cvengros@icloud.com'>
+                     <FaEnvelope/>
+                </a>
+            
+                <a  href= 'https://github.com/Laurencvengros'>
+                     <FaGithub/> 
+                </a>
+            
+                <a href= 'https://www.linkedin.com/in/lauren-cvengros-a00957249/'>
+                     <FaLinkedin/>
+                </a>
+            </div>
+    
        
     </footer>
+    </IconContext.Provider>
         </React.Fragment>
       );
     }
