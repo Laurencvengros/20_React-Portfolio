ced8173f4495eee9ec932c49313bc3cd044b2d19
Lauren <l.cvengros@icloud.com>
2022-12-07 10:54:10 -0500

    set function to display reqired field message when the user leaves field blank

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -35,13 +35,18 @@ const Contact =() =>{
     const [formState, setFormState] = useState({email: '', name: '', message: ''});
     const [contactMessage, setContactMessage]= useState('');
 
-    function handleEmail(e) {
+    function handleForm(e) {
         if (e.target.name === 'email'){
             const validEmail = validateEmail(e.target.value);
             if(!validEmail){
                 setContactMessage('Please Emter A Valid Email');
             }else{
-                setContactMessage('')
+                setContactMessage('');
+            }
+        }else{
+            if(!e.target.vaule.length){
+                const name = e.target.name;
+                setContactMessage(`${name.charAt(0).toUpperCase()+ name.slice(1)} cannot be blank`)
             }
         }
 
@@ -65,28 +70,36 @@ const Contact =() =>{
                                     type="text"
                                     placeholder="Your Name"
                                     required name="name" 
-                                    onBlur={handleEmail}
-                                ></Form.Control>
+                                    onBlur={handleForm}
+                                />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control
+                                required name="email"
+                             type="email" 
+                             placeholder="Enter email"
+                             onBlur={handleForm}
+                              />
                             <Form.Text className="text-muted"></Form.Text>
                         </Form.Group>
-    
                         <Form.Group className="mb-3" controlId="message">
                             <Form.Label>Message</Form.Label>
                             <Form.Control
+                                required name="message"
                                 type="text"
-                                placeholder="Please get in touch"
-                                name="message"
+                                placeholder="Type Your Message"
                                 as="textarea"
                                 rows={3}
+                                onBlur={handleForm} 
                             ></Form.Control>
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicCheckbox">
                          <Form.Check type="checkbox" label="Check me out" />
                         </Form.Group>
+
+                        {contactMessage && <p >{contactMessage}</p>}
+
                         <Button  variant="warning" type="submit">
                             Submit
                         </Button>
