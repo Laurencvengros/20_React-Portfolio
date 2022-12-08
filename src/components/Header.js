c3e94f050e07b29a3aac8326950e78613f8eebbb
Lauren <l.cvengros@icloud.com>
2022-12-07 20:34:41 -0500

    made the landing page the about section per the readme

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ const Header = () =>{
         <Router>
             <Navigation />
             <Routes>
-            <Route path="/"  element={<Home/>}/>
+            <Route path="/"  element={<About/>}/>
             <Route path="/about"  element={<About/>}/>
             <Route path="/home"  element={<Home/>}/>
             <Route path="/projects"  element={<Projects/>}/>
